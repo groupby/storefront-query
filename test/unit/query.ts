1a595397421f139acc8549cd1749c270780b8d0c
Ben Teichman <ben.l.teichman@gmail.com>
2017-05-23 10:49:40 -0400

    add state test for Query

--- test/unit/query.ts
+++ test/unit/query.ts
@@ -24,14 +24,42 @@ suite('Query', ({ expect, spy }) => {
     });
 
     describe('state', () => {
-      describe('submit()', () => {
-        it('should call flux.search() with the value of the first registered search-box');
+      let query: Query;
 
-        it('should not call flux.search() if no registered search-box');
+      beforeEach(() => {
+        Component.prototype.expose = () => null;
+        query = new Query();
+      });
+
+      describe('submit()', () => {
+        it('should call flux.search() with the value of the first registered search-box', () => {
+          const value = 'high tops';
+          const search = spy();
+          query.flux = <any>{ search };
+          query.registered = <any[]>[{ refs: { searchBox: { value } } }];
+
+          query.state.submit();
+
+          expect(search.calledWith(value)).to.be.true;
+        });
+
+        it('should not call flux.search() if no registered search-box', () => {
+          const search = spy();
+          query.flux = <any>{ search: () => expect.fail() };
+          query.registered = [];
+
+          query.state.submit();
+        });
       });
 
       describe('register()', () => {
-        it('should add to registered');
+        it('should add to registered', () => {
+          const tag: any = { a: 'b' };
+
+          query.state.register(tag);
+
+          expect(query.registered).to.eql([tag]);
+        });
       });
     });
   });
