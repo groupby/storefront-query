f3b87b89f81c96efd44207bed541aa57d7643da6
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-01 09:25:38 -0400

    test(package): switch to @storefront/testing

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,10 +1,9 @@
+import { bootstrap } from '@storefront/testing';
 import * as chai from 'chai';
-import * as mock from 'mock-require';
-import * as sinonChai from 'sinon-chai';
 
-chai.use(sinonChai);
-
-mock('../src/query/index.html', {});
-mock('../src/reset/index.html', {});
-mock('../src/search-box/index.html', {});
-mock('../src/submit/index.html', {});
+bootstrap(chai, __dirname, [
+  '../src/query/index.html',
+  '../src/reset/index.html',
+  '../src/search-box/index.html',
+  '../src/submit/index.html',
+]);
