9de2f3bc5fbbf1c9eac2a4ab5b2afac0fbaa8572
Ben Teichman <ben.l.teichman@gmail.com>
2017-09-08 16:10:42 -0400

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import Query from './query';
-import Reset from './reset';
 import SearchBox from './search-box';
 import Submit from './submit';
 
-export { Query, Reset, SearchBox, Submit };
+export { Query, SearchBox, Submit };
--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -3,7 +3,6 @@ import * as chai from 'chai';
 
 bootstrap(chai, __dirname, [
   '../src/query/index.html',
-  '../src/reset/index.html',
   '../src/search-box/index.html',
   '../src/submit/index.html',
 ]);
