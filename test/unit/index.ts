bd0ada1abde393a5d916f82bf4203b256b9ef58b
Ben Teichman <ben.l.teichman@gmail.com>
2017-09-11 14:01:07 -0400

    feat(BBD-707): remove gb-reset component (#24)

--- test/unit/index.ts
+++ test/unit/index.ts
@@ -1,6 +1,5 @@
 import * as pkg from '../../src';
 import Query from '../../src/query';
-import Reset from '../../src/reset';
 import SearchBox from '../../src/search-box';
 import Submit from '../../src/submit';
 import suite from './_suite';
@@ -8,10 +7,6 @@ import suite from './_suite';
 suite('package', ({ expect }) => {
   it('should expose Query', () => {
     expect(pkg.Query).to.eq(Query);
-  });
-
-  it('should expose Reset', () => {
-    expect(pkg.Reset).to.eq(Reset);
   });
 
   it('should expose SearchBox', () => {
