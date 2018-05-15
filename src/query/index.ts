39abf3090a94723275ed666ed9d494e69313a630
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-15 01:41:18 -0400

    feat(aliasing v3): improve performance of aliasing and re-rendering (#35)

--- src/query/index.ts
+++ src/query/index.ts
@@ -1,28 +1,27 @@
-import { alias, configurable, origin, tag, Events, Tag } from '@storefront/core';
+import { configurable, origin, provide, tag, Events, Tag } from '@storefront/core';
 import SearchBox from '../search-box';
 
 @configurable
-@alias('query')
+@provide('query')
 @origin('search')
 @tag('gb-query', require('./index.html'))
 class Query {
-
   registered: SearchBox[] = [];
   props: Query.Props = {
     mode: 'default',
-    showSayt: true
+    showSayt: true,
   };
   state: Query.State = {
-    register: (tag) => this.registered.push(tag),
+    register: (_tag) => this.registered.push(_tag),
     submit: () => {
       if (this.registered.length !== 0) {
         this.actions.search(this.registered[0].refs.searchBox.value);
       }
-    }
+    },
   };
 }
 
-interface Query extends Tag<Query.Props, Query.State> { }
+interface Query extends Tag<Query.Props, Query.State> {}
 namespace Query {
   export interface Props extends Tag.Props {
     // TODO handle other modes
--- src/submit/index.ts
+++ src/submit/index.ts
@@ -1,17 +1,16 @@
-import { alias, tag, Tag } from '@storefront/core';
+import { provide, tag, Tag } from '@storefront/core';
 import Query from '../query';
 
-@alias('submit')
+@provide('submit')
 @tag('gb-submit', require('./index.html'))
 class Submit {
-
   $query: Query.State;
   state: Submit.State = {
-    onClick: () => this.$query && this.$query.submit()
+    onClick: () => this.$query && this.$query.submit(),
   };
 }
 
-interface Submit extends Tag<any, Submit.State> { }
+interface Submit extends Tag<any, Submit.State> {}
 namespace Submit {
   export interface State {
     onClick(): void;
